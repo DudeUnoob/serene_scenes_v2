875e4803d5fd5f1f18fbcfb7489d3b1241e69175
Damodar Kamani <44341783+DudeUnoob@users.noreply.github.com>
2023-09-02 14:09:16 -0500

    update

--- client/src/functions/validateAuth.js
+++ client/src/functions/validateAuth.js
@@ -2,9 +2,10 @@ import axios from "axios"
 
 export async function validateAuth (token) {
 
-    const res = await axios.post("http://localhost:3000/api/v1/auth", {}, {
+    const res = await axios.post("http://localhost:3000/api/v1/google_auth", {}, {
         headers : {"Authorization": `Bearer ${token}`, "Content-Type":"application/json"}
     })
-   
+    
+    console.log(res.data)
     return res.data
 }
--- server/router/api.js
+++ server/router/api.js
@@ -2,7 +2,7 @@ const express = require("express")
 const router = express.Router()
 const decodeToken = require("../middleware/auth")
 
-router.post('/auth', (req, res, next) => {
+router.post('/google_auth', (req, res, next) => {
     decodeToken(req, res, next)
 }, (req, res, next) => {
     return res.json({ statusCode: 200, message: "Authorized" })
